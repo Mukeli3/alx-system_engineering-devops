d0721921527b6bc81fe49cc75f830b7322071a81
Mukeli3 <mukelicynthia20@gmail.com>
2024-01-19 19:59:38 +0000

    install packages

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,17 @@
+#!/usr/bin/pup
+# Installing flask from pip3 using Puppet
+package { 'python3-pip':
+ensure 'installed',
+}
+
+package { 'flask':
+ensure   => '2.1.0',
+provider => 'pip',
+require  => Package[python3-pip],
+}
+
+package { 'werkzeug':
+ensure   => '2.0.1',
+provider => 'pip',
+require  => Package['python3-pip'],
+}
