d42db2c3466d25347f83ce32160ccfff51a826f0
Mukeli3 <mukelicynthia20@gmail.com>
2024-01-19 20:36:14 +0000

    ?..

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,12 +6,12 @@ ensure 'installed',
 
 package { 'flask':
 ensure   => '2.1.0',
-provider => 'pip',
+provider => 'pip3',
 require  => Package['python3-pip'],
 }
 
 package { 'werkzeug':
 ensure   => '2.0.1',
-provider => 'pip',
+provider => 'pip3',
 require  => Package['python3-pip'],
 }
