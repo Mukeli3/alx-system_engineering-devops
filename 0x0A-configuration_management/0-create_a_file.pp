64ccdb233130cb9ee882bf834fcc645567281ba4
Mukeli3 <mukelicynthia20@gmail.com>
2024-01-19 20:02:13 +0000

    missing comma

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #!usr/bin/pup
 # Creating a file using Puppet
 file { '/tmp/school':
-  ensure => 'present'
-  group  => 'www-data'
-  owner  => 'www-data'
-  mode   => '0744'
+  ensure => 'present',
+  group  => 'www-data',
+  owner  => 'www-data',
+  mode   => '0744',
 }
