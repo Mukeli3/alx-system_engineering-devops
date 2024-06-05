74def46246ac28b4ba2f9349940a3d4eef645723
Mukeli3 <mukelicynthia20@gmail.com>
2024-06-05 17:03:16 +0000

    strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# Puppet script fix and automate Apache returning a 500 error
+
+$f_check = '/var/www/html/wp-settings.php'
+
+exec { 'rep_lne:
+  command => "sed -i 's/phpp/php/g' ${f_check}",
+  path => ['/bin','/usr/bin']
+}
