bfd71d29f27895f60498d5aefdaef4abd23f56f1
Mukeli3 <mukelicynthia20@gmail.com>
2024-06-05 17:43:35 +0000

    puppet-lint fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,7 +2,7 @@
 
 $f_check = '/var/www/html/wp-settings.php'
 
-exec { 'rep_lne:
-  command => "sed -i 's/phpp/php/g' ${f_check}",
-  path => ['/bin','/usr/bin']
+exec { 'rep_lne':
+    command => "sed -i 's/phpp/php/g' ${f_check}",
+    path    => ['/bin','/usr/bin']
 }
