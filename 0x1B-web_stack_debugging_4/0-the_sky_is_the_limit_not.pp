8097c1f9c31f032868b1672c33c3c15a5bfbf175
Mukeli3 <mukelicynthia20@gmail.com>
2024-06-12 22:41:38 +0000

    handle more requests

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# fix nginx to handle more trafic
+
+exec { 'nginx-fix':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# commiting changes, restart nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart'
+  path    => '/etc/init.d/'
+}
