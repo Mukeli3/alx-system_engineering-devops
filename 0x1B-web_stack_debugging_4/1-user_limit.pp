f5d5576f1f826211516c8f231ac989a9ccd21ae1
Mukeli3 <mukelicynthia20@gmail.com>
2024-06-13 11:08:12 +0000

    holberton user config

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# OS configuration enables holberton user log in and open file without errors
+
+exec { 'hfile-limit-increase':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'sfile-limit-increase':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
